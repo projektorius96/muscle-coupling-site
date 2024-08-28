8090d72a24f8de08100585f98d5131c9cc0f3117
LG <34185919+projektorius96@users.noreply.github.com>
2024-08-28 21:10:01 +0300

    rebuild 5, including rendering bugfix, see desc.
    - As titled, nevertheless I'd like to see if one bugfix does not generate another one: if it's failing, as per convention future rebuild, in this case "rebuild 6", SHOULD underline with [FAILING], otherwise with [PASSING] label in the title, if possible.

---
+++ assets/app-configs-0y85fcPK.js
@@ -0,0 +1,1 @@
+import{l as loader,C as CoreExports}from"./index-DI8pEXT0.js";const appConfigs=loader({base:CoreExports.Node,baseProps:{configs:{"globals.scalingFactor":64,"enums.colors":["red","green","blue","black"]}}});export{appConfigs as default};
