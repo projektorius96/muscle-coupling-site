c1f320e4a0243dfcfa4d0c8538fe7cdb59c08c3e
LG <34185919+projektorius96@users.noreply.github.com>
2024-08-29 20:34:29 +0300

    Testing grid-less mobile-friendly build

--- assets/configs-B2GLwpSu.js
+++
@@ -1,1 +0,0 @@
-import{l as loader,C as CoreExports}from"./index-EzmtL4RP.js";const configs=loader({base:CoreExports.Node,baseProps:{container:document.body,width:Number(window.innerWidth),height:Number(window.innerHeight),configs:{"globals.scalingFactor":64,"enums.colors":["red","green","blue","black"]}}});export{configs as default};
